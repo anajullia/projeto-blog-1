ee2eec4c9fac952c3a68c2b40770707d659fb68b
etec <etec@etec.com>
2022-11-01 10:25:07 -0300

    1

--- js/funcoes.js
+++ js/funcoes.js
@@ -10,17 +10,33 @@ xmlDoc = xmlhttp.responseXML;
 x = xmlDoc.getElementsByTagName("postagem");
 
 function funcaoConteudo(){
-    for(i = 0; i<=2; i++){
+    for(i = x.length-1; i>=0; i--){
         document.write(
         "<tr>"+
         "<td>" + x[i].getAttribute("codigo") + "</td>" +
         "<td><img src='imgs/" + x[i].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width=150> </td>" +
-        "<td>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
+        "<td><a href='postagem.html?codigo_postagem="+i+"'>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
         "<td>" + x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,150) +  "...</td>" +
         "</tr>");
     }
 }
 
+function funcaoPostagem(){
+        //armazenar a url completa ex: http://127.0.0.1/projeto-blog-1/postagem.html?codigo_postagem=1
+        url = new URL(window.location.href);
+        //armazena somente o parâmetro da url ex: codigo_postagem=1
+        parametro = url.searchParams;
+        //armazenar somente o valor do parametro da url ex: 1;
+        i = parametro.get("codigo_postagem");
+        i=0;
+        document.write(
+        "<tr>"+
+        "<td>" + x[i].getAttribute("codigo") + "</td>" +
+        "<td><img src='imgs/" + x[i].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width=150> </td>" +
+        "<td>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
+        "<td>" + x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue+  "</td>" +
+        "</tr>");
+}
 
 
 
